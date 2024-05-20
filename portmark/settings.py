bee022addaff76b4291d0bc7bf26b6b1f0306d0b
zorecart <zorevinacart@gmail.com>
2024-05-20 20:57:05 +0100

    Update settings.py

--- portmark/settings.py
+++ portmark/settings.py
@@ -25,7 +25,7 @@ ALLOWED_HOSTS = ['*']
 
 # Application definition
 INSTALLED_APPS = [
-    'admin_argon.apps.AdminArgonConfig',
+    'admin_soft.apps.AdminSoftDashboardConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
