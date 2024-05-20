0740bb34ae16b33d7bc7cbc31a8f4e4035072479
zorecart <zorevinacart@gmail.com>
2024-05-20 20:23:01 +0100

    Update apps.py

--- giftweb/apps.py
+++ giftweb/apps.py
@@ -1,9 +1,9 @@
+
+#apps.py
 from django.apps import AppConfig
 
-
 class GiftwebConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'giftweb'
 
-    #def ready(self):
-     #   import giftweb.signals 
+    def ready(self):
+        import giftweb.signals  # Import signals here
