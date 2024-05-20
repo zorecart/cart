e3bcc6aea95577bcfad571768542488aa58aac43
zorecart <zorevinacart@gmail.com>
2024-05-20 20:41:00 +0100

    Add files via upload

---
+++ admin_soft/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+from admin_soft import views
+from django.contrib.auth import views as auth_views
+from accounts.views import change_password_view
+from django.urls import include, re_path, path
+
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    re_path(r'^change-password/$', change_password_view, name='change_password'),
+    path('billing/', views.billing, name='billing'),
+    path('tables/', views.tables, name='tables'),
+    path('vr/', views.vr, name='vr'),
+    path('rtl/', views.rtl, name='rtl'),
+    path('profile/', views.profile, name='profile'),
+
+    # Authentication
+    path('accounts/login/', views.UserLoginView.as_view(), name='login'),
+    path('accounts/logout/', views.logout_view, name='logout'),
+    path('accounts/register/', views.register, name='register'),
+    path('accounts/password-change/', views.UserPasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password-change-done/', auth_views.PasswordChangeDoneView.as_view(
+        template_name='accounts/password_change_done.html'
+    ), name="password_change_done"),
+    path('accounts/password-reset/', views.UserPasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password-reset-confirm/<uidb64>/<token>/', 
+        views.UserPasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/password-reset-done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'
+    ), name='password_reset_done'),
+    path('accounts/password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'
+    ), name='password_reset_complete'),
+]
