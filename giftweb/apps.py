dd29aeea24bdbb1d6dd696327d9eb3708b78d26e
zorecart <zorevinacart@gmail.com>
2024-10-05 17:25:26 +0100

    Delete giftweb directory

--- giftweb/apps.py
+++
@@ -1,9 +0,0 @@
-
-#apps.py
-from django.apps import AppConfig
-
-class GiftwebConfig(AppConfig):
-    name = 'giftweb'
-
-    def ready(self):
-        import giftweb.signals  # Import signals here
--- giftweb/custom_filters.py
+++
@@ -1,7 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def instanceof(obj, class_name):
-    return obj.__class__.__name__ == class_name
